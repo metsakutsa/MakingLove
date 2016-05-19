bf61b38e202761fa2935a57e7acf9ca3fba71716
Raido Uudel <raido.uudel@hotmail.com>
2016-05-19 21:51:47 +0300

    Imaging update: instances
    Kind of fixes the color changing but the color is still wrong to begin
    with. Need to find source of error.

--- New/Scenes/Tutorial/tutorial.lua
+++ New/Scenes/Tutorial/tutorial.lua
@@ -3,15 +3,18 @@ require "states"
 require "fileReader"
 
 local tLines = readLines("Scenes/Tutorial/tutorial_dialogue.txt")
-local jorss = love.graphics.newImage("Scenes/Tutorial/Resources/jorss.png")
+local jorss = love.graphics.newImage("Scenes/Tutorial/Resources/jorss.jpg")
 local border = 5	--Border thickness
+
+--Create new SUIT instance because otherwise something bad happens...
+ tutorialSuit = suit.new()
+
 
 function drawTutorial()
 	--Draw image
 	love.graphics.draw(jorss, 50, winHeight/10 )
 	
 	--Draw the textbox
-	suit.layout:reset( border, winHeight - winHeight/4 + border, 0, 0 )
 	love.graphics.setColor(50,50,50)
 	love.graphics.rectangle("fill", border, winHeight - winHeight/4 + border, winWidth - border*2, winHeight/4 - border*2)
 	
@@ -20,22 +23,24 @@ function drawTutorial()
 	suit.Label(tLines[currentLine], suit.layout:row(winWidth-border*2 - 20, winHeight/4 - 60))
 	
 	--Make next and prev and skip button
-	suit.layout:reset( winWidth - 300, winHeight - border - 50, 10, 0 )
-	if suit.Button("Previous", suit.layout:col(80,30)).hit then
+	tutorialSuit.layout:reset( winWidth - 300, winHeight - border - 50, 10, 0 )
+	if tutorialSuit:Button("Previous", tutorialSuit.layout:col(80,30)).hit then
 		if tLines[currentLine - 1] ~= nil then
 			currentLine = currentLine - 1
 		end
 	end
 	
-	if suit.Button("Skip", suit.layout:col()).hit then
+	if tutorialSuit:Button("Skip", tutorialSuit.layout:col()).hit then
 		
 	end
 	
-	if suit.Button("Next", suit.layout:col()).hit then
+	if tutorialSuit:Button("Next", tutorialSuit.layout:col()).hit then
 		if tLines[currentLine + 1] ~= nil then
 			currentLine = currentLine + 1
 		end
 	end
+	
+	tutorialSuit:draw()
 end
 
 --Tutorial callbacks for the state machine
